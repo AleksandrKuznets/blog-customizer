5b64b3c7d53cd77c3f224dec69d796bfc6402e44
Aleksandr Kuznetsov <kuznetzov.kuznetsov2004@yandex.ru>
2025-09-03 00:53:38 +0300

    delete App from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState } from 'react';
+import { StrictMode } from 'react';
 
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
-
-import './styles/index.scss';
-import styles from './styles/index.module.scss';
+import { App } from './components/app/App';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
-
-const App = () => {
-	const [formState, setFormState] = useState(defaultArticleState);
-
-	return (
-		<main
-			className={styles.main}
-			style={
-				{
-					'--font-family': formState.fontFamilyOption.value,
-					'--font-size': formState.fontSizeOption.value,
-					'--font-color': formState.fontColor.value,
-					'--container-width': formState.contentWidth.value,
-					'--bg-color': formState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm onChange={setFormState} />
-			<Article />
-		</main>
-	);
-};
 
 root.render(
 	<StrictMode>
